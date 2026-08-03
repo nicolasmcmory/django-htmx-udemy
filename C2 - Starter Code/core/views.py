427f9b47169a5407f724f4ab4c6a8199f029b443
nicolasmcmory <nicolasmcmory@gmail.com>
2026-08-03 14:16:20 -0400

    Update

--- C2 - Starter Code/core/views.py
+++ C2 - Starter Code/core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     context = {}
-    return render(request, 'index.html', context)
+    return render(request, "index.html", context)
+
 
 def about(request):
     context = {}
-    return render(request, 'about.html', context)
+    return render(request, "partials/about.html#about", context)
