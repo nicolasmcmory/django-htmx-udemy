2db3fe01707e34b446adad3e6693181259b88ea2
nicolasmcmory <nicolasmcmory@gmail.com>
2026-08-06 16:13:23 -0400

    Update

--- C2 - Starter Code/core/urls.py
+++ C2 - Starter Code/core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("partials/about/", views.about, name="about"),
+    path("about/", views.about, name="about"),
+    path("products/", views.products, name="products"),
 ]
--- C2 - Starter Code/core/views.py
+++ C2 - Starter Code/core/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    context = {}
-    return render(request, "index.html", context)
+    if request.method == "POST" and "Hx-Request" in request.headers:
+        response = {"product": request.POST.get("product"), "id": 7}
+        return render(request, "partials/partials.html#products", response)
+
+    return render(request, "index.html", {})
 
 
 def about(request):
     context = {}
-    return render(request, "partials/about.html#about", context)
+    if "Hx-Request" in request.headers:
+        return render(request, "partials/partials.html#about", context)
+
+    return HttpResponse("This is the about page. It is not a partial request.")
+
+
+def products(request):
+    return render(request, "partials/partials.html#products", {})
